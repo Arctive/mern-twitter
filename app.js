a0d78a82a592c9ea6bbc529b57a385ee235d824f
Erick Santos <eksantos.jr@gmail.com>
2020-05-09 15:58:05 -0700

    Add user and tweet routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 //connect mongoDB
 mongoose
@@ -11,11 +13,17 @@ mongoose
     .catch( err => console.log(err));
 
 //routes
-app.get("/", (req, res) => res.send("New test"));
+app.get("/", (req, res) => {
+    res.send("New test");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 
 //port
 const port = process.env.PORT || 5000;
 
 //socket, listen for connections on path
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
