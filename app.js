1d53e642b3cf0ec665caccfb300badebf33e539a
Erick Santos <eksantos.jr@gmail.com>
2020-05-09 18:33:17 -0700

    Add user auth

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 //create Express server
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 const User = require('./models/User');
 
 //connect mongoDB
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then( () => console.log("Connected to MongoDB successfully"))
-    .catch( err => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then( () => console.log("Connected to MongoDB successfully"))
+.catch( err => console.log(err));
+
+//respond to json req and other software
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//passport middleware
+const passport = require('passport');
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 //routes
-app.get("/", (req, res) => {
-    const user = new User({
-        handle: "jim",
-        email: "jim@jim.jim",
-        password: "jimisgreat123"
-    })
-    user.save();
-    res.send("Hello World!");
-});
-
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
