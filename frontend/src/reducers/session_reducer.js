e4a0236d03cafca02445807aa72a39d8832a4d48
Erick Santos <eksantos.jr@gmail.com>
2020-05-10 13:11:39 -0700

    Add frontend login and signup

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,12 +1,27 @@
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { 
+    RECEIVE_CURRENT_USER,
+    RECEIVE_USER_SIGN_IN,
+    RECEIVE_USER_LOGOUT 
+} from '../actions/session_actions';
 
 const initialState = {
     isAuthenticated: false,
-    user = {}
+    user: {}
 };
 
 export default function( state = initialState, action) {
     switch (action.type){
+        case RECEIVE_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            };
+        case RECEIVE_USER_SIGN_IN:
+            return {
+                ...state,
+                isSignedIn: true
+            }
         case RECEIVE_USER_LOGOUT:
             return {
                 isAuthenticated: false,
