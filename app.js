968c2a7e1fab38884fa2b4a4a776e6f35ac81abf
Erick Santos <eksantos.jr@gmail.com>
2020-05-10 18:32:09 -0700

    Change keys

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ mongoose
 .then( () => console.log("Connected to MongoDB successfully"))
 .catch( err => console.log(err));
 
+
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+    app.user(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
 //respond to json req and other software
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,11 +42,3 @@ const port = process.env.PORT || 5000;
 
 //socket, listen for connections on path
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-const path = require('path');
-if (process.env.NODE_ENV === 'production'){
-    app.user(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    });
-}
